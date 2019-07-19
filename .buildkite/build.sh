47aec609d67bfbd509b19ed4a52ad1a8e3ea83d7
Christopher Bayliss <christopher.bayliss@unimelb.edu.au>
2019-07-19 16:59:07 +1000

    chore(ci): Updates build to use new Maven

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -12,6 +12,25 @@ function maven {
   mvn -T 1C -Djava.awt.headless=true -Paurin ${@}
 }
 
+
+echo "--- Bootstrap Build"
+
 javadoc_tools
 
+echo "--- Update Version"
+CURRENT_VERSION=$(maven org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
+NEW_VERSION="${CURRENT_VERSION}-${BUILDKITE_BUILD_NUMBER}"
+
+maven org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion="${CURRENT_VERSION}-${BUILDKITE_BUILD_NUMBER}"
+
+buildkite-agent meta-data set version "v${NEW_VERSION}"
+
+
+echo "--- Perform Build"
 maven clean install
+
+echo "--- Deploy"
+maven \
+  -DaltDeploymentRepository="new_nexus::https://repo.aurin.cloud.edu.au/repository/maven-aurin-stable/" \
+  org.apache.maven.plugins:maven-deploy-plugin:3.0.0-M1:deploy
+
