81c6988e8076160eea048d10cbccc9f11c9bbfc7
Christopher Bayliss <christopher.bayliss@unimelb.edu.au>
2019-07-22 14:02:08 +1000

    chore(ci): Improve build script

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -2,35 +2,85 @@
 
 set -euo pipefail
 
+MVN_REPOSITORY="new_nexus::default::https://repo.aurin.cloud.edu.au/repository/maven-aurin-geotools"
+MVN_EVALUATE_PLUGIN="org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate"
+BUILDKITE_BUILD_NUMBER="${BUILDKITE_BUILD_NUMBER:-1}"
+
 function javadoc_tools {
-  cd build/maven/javadoc
+  pushd build/maven/javadoc
   maven clean install
-  cd $BUILDKITE_BUILD_CHECKOUT_PATH
+  popd
 }
 
 function maven {
-  mvn -T 1C -Djava.awt.headless=true -Paurin ${@}
+  mvn -U -Djava.awt.headless=true -Paurin "${@}"
 }
 
+function set_version {
+  echo "--- Update Version"
 
-echo "--- Bootstrap Build"
+  local build_number
+  build_number="${BUILDKITE_BUILD_NUMBER}"
 
-javadoc_tools
+  local current_version
+  current_version="$(maven ${MVN_EVALUATE_PLUGIN} -Dexpression=project.version -q -DforceStdout)"
 
-echo "--- Update Version"
-CURRENT_VERSION=$(maven org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
-NEW_VERSION="${CURRENT_VERSION}-${BUILDKITE_BUILD_NUMBER}"
+  local new_version
+  new_version="${current_version}"
 
-maven org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion="${CURRENT_VERSION}-${BUILDKITE_BUILD_NUMBER}"
+  #-${BUILDKITE_BUILD_NUMBER}"
+  #maven org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion="${CURRENT_VERSION}-${BUILDKITE_BUILD_NUMBER}"
+  buildkite-agent meta-data set version "v${new_version}"
+}
 
-buildkite-agent meta-data set version "v${NEW_VERSION}"
+function mvn_bootstrap {
+  echo "--- Bootstrap Build"
+  set_version
+  javadoc_tools
+}
 
+function mvn_clean {
+  echo "--- cleaning"
+  maven clean
+}
 
-echo "--- Perform Build"
-maven clean install
+function mvn_compile {
+  echo "--- Building"
+  maven compile
+}
 
-echo "--- Deploy"
-maven \
-  -DaltDeploymentRepository="new_nexus::https://repo.aurin.cloud.edu.au/repository/maven-aurin-stable/" \
-  org.apache.maven.plugins:maven-deploy-plugin:3.0.0-M1:deploy
+function mvn_test {
+  echo "--- Testing"
+  maven test
+}
+
+function mvn_deploy {
+  local version
+  version=$(buildkite-agent meta-data get version)
+  echo "--- Deploy ${version}"
+  maven -DskipTests "-DaltDeploymentRepository=${MVN_REPOSITORY}" deploy
+}
+
+function usage() {
+ echo "Must specify a mode"
+}
+
+case $1 in
+    test)
+      mvn_clean
+      mvn_bootstrap
+      mvn_compile
+      mvn_test
+    ;;
+    deploy)
+      mvn_clean
+      mvn_bootstrap
+      mvn_compile
+      mvn_deploy
+    ;;
+    * )
+      usage
+      exit 1
+esac
+
 
